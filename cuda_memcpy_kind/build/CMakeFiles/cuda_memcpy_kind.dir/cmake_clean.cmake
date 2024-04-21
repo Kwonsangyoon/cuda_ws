100fcef9e2a1b60fea23f2d28d78c9357c6a2d0d
kwon <ksu619@naver.com>
2024-04-22 01:53:00 +0900

    cuda_memcpy_kind

---
+++ cuda_memcpy_kind/build/CMakeFiles/cuda_memcpy_kind.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_memcpy_kind.dir/cuda_memcpy_kind.cu.o"
+  "CMakeFiles/cuda_memcpy_kind.dir/cuda_memcpy_kind.cu.o.d"
+  "cuda_memcpy_kind"
+  "cuda_memcpy_kind.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cuda_memcpy_kind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cuda_memcpy_kind/build/CMakeFiles/cuda_memcpy_kindlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_memcpy_kindlib.dir/cuda_memcpy_kind.cu.o"
+  "CMakeFiles/cuda_memcpy_kindlib.dir/cuda_memcpy_kind.cu.o.d"
+  "libcuda_memcpy_kindlib.a"
+  "libcuda_memcpy_kindlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cuda_memcpy_kindlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
